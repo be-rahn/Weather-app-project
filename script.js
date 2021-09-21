07d7e12ff06d188db05ab5979ccac78031a0ae9f
Brooke Rahn <brooke.rahn@gmail.com>
2021-09-21 14:40:06 -0700

    add weather icon

--- script.js
+++ script.js
@@ -26,6 +26,11 @@ function showTemp(response) {
   );
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function searchLocation(position) {
